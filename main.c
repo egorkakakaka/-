ebcbf02242371cf35faf9cecdc72f7fd32db207f
Егор <46293636+egorkakakaka@users.noreply.github.com>
2020-06-16 16:30:40 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,30 +1,46 @@
 // Лабараторная работа по графам
 // Работу выполнил Парамонов Егор Сергеевич ИВТ13-БО
-// На вход поступает матрица сежности, программа проверяет есть ли в данном графе циклы или их нет
+// На вход поступает матрица смежности, программа проверяет есть ли в данном графе циклы или их нет
 #include <stdio.h>
 #include <stdlib.h>
 #include "proverka.h"
 
 int main()
 {
-	int i, j, s;
-	int* a;
+	int i, j, graph_size;
+	int* arr;
 	printf("How many vertices you have in your graph?");
 	printf("\n");
-	scanf_s("%d", &s); // количество вершин графа
-	a = malloc(sizeof(int) * s * s); // выделяем память
+	// количество вершин графа
+	if(scanf_s("%d", &graph_size)!=1)
+	{
+		printf("Error symbol");
+		return 0;
+	}
+	// выделяем память под массив
+	arr = malloc(sizeof(int) * graph_size * graph_size); 
+	if (arr == NULL)
+	{
+		printf("Error memmory");
+		return 0;
+	}
 	printf("Enter the adjacency table.");
 	printf("\n");
 	int start = 0;
-	for (i = 0; i < s; i++) // ввод смежной матрицы
-		for (j = 0; j < s; j++)
-			scanf_s("%d", (a + i * s + j));
-	int result = proverka(a, 0, s, 0, 0); // если вернется 1 то значит петля есть если 0 то петли нет
+	for (i = 0; i < graph_size; i++) // ввод смежной матрицы
+		for (j = 0; j < graph_size; j++)
+	// проверяем на правильность ввода матрицу смежности
+			if((scanf_s("%d", (arr + i * graph_size + j))!=1) || (*(arr + i * graph_size + j))!=1 || (*(arr + i * graph_size + j)) != 1)
+			{
+				printf("Error symbol");
+				return 0;
+			}
+	int result = proverka(arr, 0, graph_size, 0, 0); // если вернется 1 то значит петля есть если 0 то петли нет
 	if (result)
 		printf("Yes");
 	else
 		printf("No");
 
-	free(a);
+	free(arr);
 	return 0;
 }
