5a3eb1a35280c0acce15fa3bb7932d0dd70ffe25
Егор <46293636+egorkakakaka@users.noreply.github.com>
2020-06-23 15:21:04 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,44 +3,41 @@
 // На вход поступает матрица смежности, программа проверяет есть ли в данном графе циклы или их нет
 #include <stdio.h>
 #include <stdlib.h>
-#include "proverka.h"
+#include "input_size.h"
+#include "memmory_check.h"
+#include "input_graph.h"
+#include "graph_check.h"
+#include "answer_check.h"
 
 int main()
 {
-	int i, j, graph_size;
+	int i, graph_size = 0, result = 0, start = 0;
 	int* arr;
-	printf("How many vertices you have in your graph?");
-	printf("\n");
+	int* arr_2;
 	// количество вершин графа
-	if(scanf_s("%d", &graph_size)!=1)
-	{
-		printf("Error symbol");
-		return 0;
-	}
+	graph_size = input_size();
 	// выделяем память под массив
-	arr = malloc(sizeof(int) * graph_size * graph_size); 
-	if (arr == NULL)
-	{
-		printf("Error memmory");
-		return 0;
-	}
-	printf("Enter the adjacency table.");
-	printf("\n");
-	int start = 0;
-	for (i = 0; i < graph_size; i++) // ввод смежной матрицы
-		for (j = 0; j < graph_size; j++)
-	// проверяем на правильность ввода матрицу смежности
-			if((scanf_s("%d", (arr + i * graph_size + j))!=1) || (*(arr + i * graph_size + j))!=1 || (*(arr + i * graph_size + j)) != 1)
-			{
-				printf("Error symbol");
-				return 0;
-			}
-	int result = proverka(arr, 0, graph_size, 0, 0); // если вернется 1 то значит петля есть если 0 то петли нет
-	if (result)
+	arr = (int*) malloc(sizeof(int) * graph_size * graph_size); 
+	arr_2 = (int*) malloc(graph_size * sizeof(int));
+	memmory_check(arr, graph_size);
+	memmory_check(arr_2, graph_size);
+	// ввод графа через таблицу смежности
+	input_graph(graph_size, arr);
+	// проверка на правильность ввода графа
+	graph_check(arr, graph_size);
+	// заполняем массив нулями, в котором будем помечать вершины, в которых мы уже были
+	for (i = 0; i < graph_size; i++)
+		arr_2[i] = 0;
+	//запускаем проверку на циклы
+	answer_check(arr, arr_2, 0, graph_size, 0, 0, &result);
+	// если граф не связный то проверяем те вершины в которых мы еще не были
+	for (i = 0; i < graph_size; i++)
+		if (arr_2[i] == 0)
+			answer_check(arr, arr_2, i, graph_size, 0, i, &result);
+	if (result!=0)
 		printf("Yes");
 	else
 		printf("No");
-
 	free(arr);
 	return 0;
 }
