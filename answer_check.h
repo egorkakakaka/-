6b1672d14b15792343d28a2203f27d0dfefd7888
Егор <46293636+egorkakakaka@users.noreply.github.com>
2020-06-23 15:22:24 +0300

    Update and rename proverka.h to answer_check.h

---
+++ answer_check.h
@@ -0,0 +1,2 @@
+#pragma once
+int answer_check(int* arr, int* arr_2, int current, int graph_size, int last, int start , int* result);
--- proverka.h
+++
@@ -1,2 +0,0 @@
-#pragma once
-int proverka(int* arr, int current, int graph_size, int last, int start);
