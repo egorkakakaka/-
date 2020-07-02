c1effbba617d7be4c26eca8f04d2c39bee53d984
Егор <46293636+egorkakakaka@users.noreply.github.com>
2020-07-02 13:04:48 +0300

    Create utl.h

---
+++ utl.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "stk.h"
+int memmory_check(Stk* stack);
+int input_size();
+int input_str(Stk* stack, int arr_size, char* arr);
+int arr_check(char* arr, int arr_size, Stk* stack);
+int output(char* answer, int arr_size);
