106ea3106c724490f963a9d750f67e38203d1c2f
Егор <46293636+egorkakakaka@users.noreply.github.com>
2020-07-02 13:05:46 +0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,28 @@
+// Лабораторная работа по теме Стэк и Очередь
+// Выполнил Парамонов Егор Сергеевич ИВТ13-БО
+// Программа переводит выражение в обратную польскую запись
+#include <stdio.h>
+#include <stdlib.h>
+#include "stk.h"
+#include "utl.h"
+#include "func.h"
+
+int main()
+{
+	int i = 0, arr_size=0, j = 0;
+	char arr[40];
+	char answer[40];
+	int check = 0;
+	Stk* stack = NULL;
+	// количество символов в массиве
+	arr_size = input_size();
+	stack = malloc(sizeof(Stk));
+	memmory_check(stack);
+	input_str(stack, arr_size, arr);
+	start(stack);
+	arr_check(arr, arr_size, stack);
+	func(stack, arr, arr_size, answer);
+	output(answer, arr_size);
+	free(stack);
+	return 0;
+}
