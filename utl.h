a2b94a203eff8ae41bba38791d2ca936c238e9c9
Егор <46293636+egorkakakaka@users.noreply.github.com>
2020-07-02 15:57:02 +0300

    Update utl.h

--- utl.h
+++ utl.h
@@ -2,6 +2,6 @@
 #include "stk.h"
 int memmory_check(Stk* stack);
 int input_size();
-int input_str(Stk* stack, int arr_size, char* arr);
+int input_str(int arr_size, char* arr);
 int arr_check(char* arr, int arr_size, Stk* stack);
 int output(char* answer, int arr_size);
