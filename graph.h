36172be60c27615cce5bbabe3688fc76ee76e234
Егор <46293636+egorkakakaka@users.noreply.github.com>
2020-07-02 08:47:27 +0300

    Create graph.h

---
+++ graph.h
@@ -0,0 +1,4 @@
+#pragma once
+int graph_check(int* arr, int graph_size);
+int input_graph(int graph_size, int* arr);
+int answer_check(int* arr, int* arr_2, int current, int graph_size, int last, int start);
