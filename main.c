695f42929fbddb8e52705a95551fce9ade15ce91
Егор <46293636+egorkakakaka@users.noreply.github.com>
2020-07-02 08:49:05 +0300

    Update main.c

--- main.c
+++ main.c
@@ -3,11 +3,9 @@
 // На вход поступает матрица смежности, программа проверяет есть ли в данном графе циклы или их нет
 #include <stdio.h>
 #include <stdlib.h>
-#include "input_size.h"
-#include "memmory_check.h"
-#include "input_graph.h"
-#include "graph_check.h"
-#include "answer_check.h"
+#include "Utils.h"
+#include "graph.h"
+
 
 int main()
 {
@@ -17,8 +15,8 @@ int main()
 	// количество вершин графа
 	graph_size = input_size();
 	// выделяем память под массив
-	arr = (int*) malloc(sizeof(int) * graph_size * graph_size); 
-	arr_2 = (int*) malloc(graph_size * sizeof(int));
+	arr = (int*)malloc(sizeof(int) * graph_size * graph_size);
+	arr_2 = (int*)malloc(graph_size * sizeof(int));
 	memmory_check(arr, graph_size);
 	memmory_check(arr_2, graph_size);
 	// ввод графа через таблицу смежности
@@ -28,16 +26,15 @@ int main()
 	// заполняем массив нулями, в котором будем помечать вершины, в которых мы уже были
 	for (i = 0; i < graph_size; i++)
 		arr_2[i] = 0;
-	//запускаем проверку на циклы
-	answer_check(arr, arr_2, 0, graph_size, 0, 0, &result);
 	// если граф не связный то проверяем те вершины в которых мы еще не были
 	for (i = 0; i < graph_size; i++)
 		if (arr_2[i] == 0)
-			answer_check(arr, arr_2, i, graph_size, 0, i, &result);
-	if (result!=0)
+			result += answer_check(arr, arr_2, i, graph_size, 0, i);
+	if(result != 0)
 		printf("Yes");
-	else
+	if (result==0)
 		printf("No");
 	free(arr);
+	free(arr_2);
 	return 0;
 }
