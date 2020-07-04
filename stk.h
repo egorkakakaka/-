3af5aa3cf8deb2f6691bc3f6c4aaf0b5fddb2c5a
Егор <46293636+egorkakakaka@users.noreply.github.com>
2020-07-04 15:40:03 +0300

    Update stk.h

--- stk.h
+++ stk.h
@@ -1,10 +1,14 @@
 #pragma once
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct Stack
 {
-	char data[40];
+	char* data;
 	int curr;
 } Stk;
-void start(Stk* stack);
+
+void start(Stk* stack, int arr_size);
 void push(Stk* stack, char symbol, int arr_size);
 int pop(Stk* stack);
 int top(Stk* stack);
