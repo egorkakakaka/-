dc543dda0a07ddc9e5cb605aecead945dab3d504
Егор <46293636+egorkakakaka@users.noreply.github.com>
2020-07-04 15:34:42 +0300

    Update main.c

--- main.c
+++ main.c
@@ -9,20 +9,21 @@
 
 int main()
 {
-	int i = 0, arr_size=0, j = 0;
+	int i = 0, j = 0;
+	int arr_size=0;
 	char* arr = NULL;
 	char* answer = NULL;
 	Stk* stack = NULL;
 	// количество символов в массиве
 	arr_size = input_size();
 	stack = malloc(sizeof(Stk));
-	arr = malloc(sizeof(Stk)*arr_size);
-	answer = malloc(sizeof(Stk)*arr_size);
+	arr = malloc(sizeof(char) * arr_size);
+	answer = malloc(sizeof(char) * arr_size);
 	memmory_check(stack);
 	memmory_check(arr);
 	memmory_check(answer);
 	input_str(arr_size, arr);
-	start(stack);
+	start(stack, arr_size);
 	reverse_polish_notation(stack, arr, arr_size, answer);
 	output(answer, arr_size);
 	free(arr);
