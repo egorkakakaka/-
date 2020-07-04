ac1039ac3e45d720ab3da4f3b0962627a5e1393a
Егор <46293636+egorkakakaka@users.noreply.github.com>
2020-07-04 14:41:19 +0300

    Update utl.h

--- utl.h
+++ utl.h
@@ -3,8 +3,8 @@
 int memmory_check(Stk* stack);
 int input_size();
 int input_str(int arr_size, char* arr);
-int arr_check_1(char* arr, int arr_size);
+int arr_check_symbol(char* arr, int arr_size);
 int output(char* answer, int arr_size);
-int arr_check_2(char* arr, int arr_size);
-int arr_check_3(char* arr, int arr_size);
-int arr_check_4(char* arr, int arr_size);
+int arr_check_sin_cos(char* arr, int arr_size);
+int arr_check_brackets(char* arr, int arr_size);
+int arr_check_sentence(char* arr, int arr_size);
